ded95bae5e4cedabcd12e40de85c857cb895ea28
John Amenta <johnamenta5@gmail.com>
2016-10-21 18:15:13 -0400

    1) Add firebase connection 2) Adjust CSS to allow for mobile usage 3) Add favicon icon to title bar

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,6 +1,6 @@
-var firebaseConfig = {
+var config = {
     apiKey: "AIzaSyAQq3H4ybMJSynsNPESz3nkNpIiRqSJhfI",
     authDomain: "groomsmen-fde65.firebaseapp.com",
-    databaseURL: "https://groomsmen-fde65.firebaseio.com",
-    storageBucket: "groomsmen-fde65.appspot.com",
+    databaseURL: "https://groomsmen-fde65.firebaseio.com/",
+	storageBucket: "groomsmen-fde65.appspot.com"
   };
--- public/js/groomsmen.js
+++ public/js/groomsmen.js
@@ -2,9 +2,19 @@ $(document).ready(function(){
 	$('#termsCheckbox').change(function () {
 		$('#submitButton').prop("disabled", !this.checked);
 	}).change()
-})
+})  
 
 function handleClick() {
 	firebase.initializeApp(config);
+	var data = ({
+		name: "Nick",
+		response: true
+	});
+	var pushEntry = firebase.database().ref('Responses/').push();
+	pushEntry.set({
+		name: "Nick",
+		response: true
+	});
+	console.log(data);
 	location.href = "success.html";
 }
